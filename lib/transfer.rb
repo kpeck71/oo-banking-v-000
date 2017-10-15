a769e81faaa3f5f10a38718f5a0fdf5f5c6b7119
Kristen Peck <kpeck71@gmail.com>
2017-10-15 15:00:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status = "complete"
+    if valid? && self.status = "complete"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "reversed"
