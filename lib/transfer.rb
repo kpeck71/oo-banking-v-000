73383244ea54e34958e3104b2456d0385b6636c0
Kristen Peck <kpeck71@gmail.com>
2017-10-15 14:59:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && sender.balance > amount
       receiver.balance += amount
       sender.balance -= amount
       self.status = "complete"
