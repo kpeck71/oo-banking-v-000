4dee4d7ddf149aedf2dda98ee54af6d90ca21986
Kristen Peck <kpeck71@gmail.com>
2017-10-15 14:57:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,7 @@ class Transfer
   def execute_transaction
     if valid?
       receiver.balance += amount
+      sender.balance -+ amount
       status = "complete"
     else
       status = "rejected"
